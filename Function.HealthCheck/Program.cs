3ef63f3a3a4562688702b9320d12b9cf061d4edc
Juan Ramos <ptjuanramos@gmail.com>
2022-02-21 11:56:28 +0000

    Add OpenIdConnectService health check example. Add signalr health check example(HealthChecks.SignalR nuget doesn't work that well).

--- Function.HealthCheck/Program.cs
+++ Function.HealthCheck/Program.cs
@@ -13,8 +13,9 @@ namespace Function.HealthCheck
                 .ConfigureServices(collection =>
                 {
                     collection.AddHealthChecks()
+                        .AddIdentityServer(new Uri("https://[url]/identity/"), "IdentityService-check")
                         .AddUrlGroup(new Uri("http://httpbin.org/status/200"), "DependencyService-check")
-                        .AddUrlGroup(new Uri("https://[...]/identity/.well-known/openid-configuration"), "IdentityService-check");
+                        .AddUrlGroup(new Uri("https://[service-url]/api/v1/health"), "SignalRService-check");
                 })
                 .Build();
 
